91f76df2b48fb1659e1f0eca90a3790299d3550a
curso06 <nocorreo@mocorreo.com>
2025-02-11 11:54:48 +0100

    inicializar variables en el application properties

--- src/test/java/es/santander/ascender/ejerc002/DatoRepositoryTest.java
+++ src/test/java/es/santander/ascender/ejerc002/DatoRepositoryTest.java
@@ -1,12 +1,16 @@
 package es.santander.ascender.ejerc002;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -14,6 +18,10 @@ public class DatoRepositoryTest {
 
     @Autowired
     DatoRepository datoRepository;
+
+    // Asignar un valor al crear el objeto
+    @Value(value = "${cadena}")
+    private String cadena;
 
     @Test
     public void testList() {
@@ -34,6 +42,12 @@ public class DatoRepositoryTest {
         datoRepository.save(datoAGuardar);
 
         Optional<Dato> resultado = datoRepository.findById(datoAGuardar.getId());
-        assertTrue(resultado.isEmpty());
+        assertFalse(resultado.isEmpty());
+    }
+
+    @Test
+    public void testCadenaComprobar() {
+        assertNotNull(cadena);
+        assertEquals(cadena, "Hola Juan");
     }
 }
