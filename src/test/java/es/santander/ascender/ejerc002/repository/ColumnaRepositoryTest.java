186c15113dcb69d6a00cd877098b27e3a341266b
curso06 <nocorreo@mocorreo.com>
2025-02-13 12:10:35 +0100

    nuevos tests para el CRUD

--- src/test/java/es/santander/ascender/ejerc002/repository/ColumnaRepositoryTest.java
+++ src/test/java/es/santander/ascender/ejerc002/repository/ColumnaRepositoryTest.java
@@ -1,6 +1,7 @@
 package es.santander.ascender.ejerc002.repository;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
@@ -8,6 +9,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.santander.ascender.ejerc002.model.Columna;
 
@@ -38,6 +40,20 @@ public class ColumnaRepositoryTest {
     }
 
     @Test
+    public void createPeroNoLlegaAlValorMinimoDeA2() {
+
+        Columna columna = new Columna();
+        // a2 tiene que ser mayor o igual que 5 por validaciones
+        columna.setA2(4);
+        // a1 tiene que ser distinto de null
+        columna.setA1(4l);
+
+        // Tengo un objeto y quiero la clase => Objeto.class
+        // Cuando quiero comprobar que lanza una exception (TransactionSystemException)
+        assertThrows(Exception.class, () -> repository.save(columna));
+    }
+
+    @Test
     public void delete() {
 
         Columna columna = new Columna();
@@ -55,7 +71,7 @@ public class ColumnaRepositoryTest {
 
     @Test
     public void view() {
-        
+
         Columna columna = new Columna();
         columna.setA2(8);
         columna.setA1(30L);
@@ -69,7 +85,7 @@ public class ColumnaRepositoryTest {
 
     @Test
     public void update() {
-    
+
         Columna columna = new Columna();
         columna.setA2(7);
         columna.setA1(20L);
@@ -79,6 +95,38 @@ public class ColumnaRepositoryTest {
 
         columna.setA2(10);
         repository.save(columna);
+
+        Optional<Columna> updatedColumna = repository.findById(columna.getId());
+
+        assertTrue(updatedColumna.isPresent());
+        assertTrue(updatedColumna.get().getA2() == 10);
+    }
+
+    @Test
+    @Transactional
+    // Forma de hacer el update sin utilizar el save
+    public void updateRaro() {
+
+        Columna columna = new Columna();
+        columna.setA2(7);
+        columna.setA1(20L);
+        repository.save(columna);
+
+        assertTrue(repository.existsById(columna.getId()));
+
+        columna.setA2(10);
+
+        /*
+         * Cuando pides columna a traves de JPA es una variable de datos
+         * 
+         * Al estar en la misma transacción, con flush ejecuta los cambios pendientes y
+         * hace el update en BBDD del nuevo valor
+         * 
+         * cambias el 7 por el 10 en memoria. Con flush detecta que los valores no
+         * coinciden y cambia el valor en la BBDD
+         */
+
+        repository.flush();
 
         Optional<Columna> updatedColumna = repository.findById(columna.getId());
 
